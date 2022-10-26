5fef57ff0c4a267493e3c3047158b0348965b894
SeolYoungKim <nasur4da@gmail.com>
2022-10-26 13:53:05 +0900

    test

--- src/main/java/item5/factorymethod/DefaultAppleSiliconChipFactory.java
+++ src/main/java/item5/factorymethod/DefaultAppleSiliconChipFactory.java
@@ -1,11 +1,12 @@
 package item5.factorymethod;
 
 import item5.di.AppleSiliconChip;
+import item5.di.M1;
 
 public class DefaultAppleSiliconChipFactory implements AppleSiliconChipFactory{
 
     @Override
     public AppleSiliconChip getAppleSiliconChip() {
-        return null;
+        return new M1();
     }
 }
