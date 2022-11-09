f3a4ba11723dcd08032f5d79414e508567c03ce4
SeolYoungKim <nasur4da@gmail.com>
2022-11-10 02:36:42 +0900

    enumtest

---
+++ src/main/java/test/Results.java
@@ -0,0 +1,61 @@
+package test;
+
+public class Results {
+
+    public enum LottoResults {
+        FIFTH(
+                3,
+                false,
+                5_000
+        ),
+        FORTH(
+                4,
+                false,
+                50_000
+        ),
+        THIRD(
+                5,
+                false,
+                1_500_000
+        ),
+        SECOND(
+                5,
+                true,
+                30_000_000
+        ),
+        FIRST(
+                6,
+                false,
+                2_000_000_000
+        );
+
+        private final int matchNumberCount;
+        private final boolean matchBonusNumber;
+        private final int winnings;
+
+        LottoResults(int matchNumberCount, boolean matchBonusNumber, int winnings) {
+            this.matchNumberCount = matchNumberCount;
+            this.matchBonusNumber = matchBonusNumber;
+            this.winnings = winnings;
+        }
+
+        public String stringFormat() {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder
+                    .append(matchNumberCount)
+                    .append("개 일치");
+
+            if (matchBonusNumber) {
+                stringBuilder
+                        .append(", 보너스 볼 일치");
+            }
+
+            stringBuilder
+                    .append(" (")
+                    .append(winnings)
+                    .append("원)");
+
+            return stringBuilder.toString();
+        }
+    }
+}
