ad9f6c4fe638c626d0287a54cedc7c46c6d8e5f9
SeolYoungKim <nasur4da@gmail.com>
2022-11-13 20:53:11 +0900

    item21 add

---
+++ src/main/java/item21/GeumZzocks.java
@@ -0,0 +1,9 @@
+package item21;
+
+public class GeumZzocks extends Mother implements Father {
+
+    public static void main(String[] args) {
+        GeumZzocks geumZzocks = new GeumZzocks();
+        geumZzocks.smashed();
+    }
+}
