41db113fc5c3f68f080f7873d3e47415f180f633
SeolYoungKim <nasur4da@gmail.com>
2023-01-07 14:09:07 +0900

    item64 add

---
+++ src/main/java/chapter_09/item64/Main.java
@@ -0,0 +1,11 @@
+package chapter_09.item64;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> badCase = new LinkedHashSet<>();
+        Set<Integer> goodCase = new LinkedHashSet<>();
+    }
+}
